109a4df6bf8a825f1d980537cb7e0bec9e3e2399
nicolas10ramirez <91766553+nicolas10ramirez@users.noreply.github.com>
2021-10-01 22:53:13 -0300

    api v1
    conexion con arduino y bd

---
+++ apiArduino/servidoriot/conexion.php
@@ -0,0 +1,21 @@
+<?php
+
+class conexion{
+
+    const user='id17674836_agronomy';
+    const pass='j*[MSTUO-c5Sd9%F';
+    const db='id17674836_smart';
+    const servidor='localhost'; 
+
+    public function conectardb(){
+        $conectar= new mysqli(self::servidor, self::user, self::pass, self::db);
+        if($conectar->connect_errno){
+            die("Error en la coneccion".$conectar->connect_error);
+        }
+        return $conectar;
+
+    }
+
+}
+
+?>
---
+++ apiArduino/servidoriot/prueba_recibe.php
@@ -0,0 +1,39 @@
+<?php
+
+    require_once('conexion.php');
+
+    $device = $_GET['divice_label'];
+    $temperature = $_GET['temperature'];
+    $humidity = $_GET['humidity'];
+
+    $conn = new conexion();
+
+    $query = "SELECT * FROM registros_monitorio";
+    $select = mysqli_query($conn->conectardb(),$query);
+
+    if($select->num_rows){
+
+        
+        $query = "INSERT INTO `registros_monitorio` (`idregistros_monitorio`, `idsensores`, `idtipo_registro`, `registros_monitorio`, `fecha`, `hora`, `estado`) 
+        VALUES (NULL, '1', '1', $humidity , now(), now(), '1');";
+
+        //         $query = "INSERT INTO `registros_monitorio` (`idregistros_monitorio`, `idsensores`, `idtipo_registro`, `registros_monitoriocol`, `estado`) 
+        // VALUES (NULL, '1', '1', $temperature, '1');";
+
+        // $query = "UPDATE device_state SET temperatura = $temperature, humedad=$humidity WHERE idDevice = '$device'";
+
+        $insert = mysqli_query($conn->conectardb(),$query);
+
+        // $query = "INSERT INTO device_historic(idDevice, variable, valor, fecha) VALUES ('$device','temperatura','$temperature',NOW())";
+        // $insert = mysqli_query($conn->conectardb(),$query);
+        // $query = "INSERT INTO device_historic(idDevice, variable, valor, fecha) VALUES ('$device','humedad','$humidity',NOW())";
+        // $insert = mysqli_query($conn->conectardb(),$query);
+        echo "***DATOS REGISTRADOS***<br>";
+        echo"{DEVICE:".$device.", TEMP:".$temperature.", HUM:".$humidity."}";
+
+    }else{
+        echo "***LA TARJETA NO EXISTE***<br>";
+    }
+
+    
+?>
